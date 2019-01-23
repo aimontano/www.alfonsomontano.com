5313293879c001074caeb094e00ec887051a681e
aimontano <aimorales.montano@gmail.com>
2019-01-22 23:33:19 -0500

    Added navigation buttons

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -4,13 +4,15 @@ import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import { IconButton } from "@material-ui/core";
+import { Home, Folder, Phone } from "@material-ui/icons";
 
 const styles = {
 	root: {
 		flexGrow: 1
 	},
-	center: {
-		// margin: "auto"
+	grow: {
+		flexGrow: 1
 	},
 	bar: {
 		background: "#2196f3"
@@ -21,11 +23,20 @@ function Navigation(props) {
 	const { classes } = props;
 	return (
 		<div className={classes.root}>
-			<AppBar position="static" style={styles.bar}>
-				<Toolbar>
-					<Typography variant="h5" color="inherit" className={classes.center}>
+			<AppBar position="static" className={classes.bar}>
+				<Toolbar variant="dense">
+					<Typography variant="h5" color="inherit" className={classes.grow}>
 						Alfonso Montano
 					</Typography>
+					<IconButton color="inherit">
+						<Home />
+					</IconButton>
+					<IconButton color="inherit">
+						<Folder />
+					</IconButton>
+					<IconButton color="inherit">
+						<Phone />
+					</IconButton>
 				</Toolbar>
 			</AppBar>
 		</div>
